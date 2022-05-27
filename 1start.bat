671eb78d34cd29e047e4a1ed7abe05629bd0c45a
wyf15927690137 <weiyanfei@sjtu.edu.cn>
2022-05-27 20:08:37 +0800

    local

---
+++ 1start.bat
@@ -0,0 +1,10 @@
+@echo off
+echo test
+start H:\learning\coding\learncpp
+set str1=This is a test string
+echo %str1%
+echo ---------------
+echo %0
+echo %~dp0
+echo ---------------
+python ../learnpy/14stringmethod.py
